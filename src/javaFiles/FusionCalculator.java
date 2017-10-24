f713ac3047654fdb30ab76779895200bb668e9fb
mruo96 <33015392+mruo96@users.noreply.github.com>
2017-10-23 21:30:47 -0700

    Update FusionCalculator.java

--- src/javaFiles/FusionCalculator.java
+++ src/javaFiles/FusionCalculator.java
@@ -5,6 +5,35 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
+/**
+ * This program is a calculator for the fusion of personas in the game Persona 5. 
+ * 
+ * About Persona 5 and Persona Fusion:
+ * Persona 5 was developed by Atlus and released in September 2016 for the Playstation 4 and
+ * Playstation 3. In Persona 5, the player collects different “persona,” which are based off
+ * of characters from myths or deities from numerous religions. Each persona can be identified
+ * by its name, its base level, and its arcana. There are 20 different arcanas 
+ * (ex. Sun, Fool, Emperor, Chariot) and around 10 personas belong to each arcana. 
+ * 
+ * Players can fuse together different personas to create a new one. Generally, the results of
+ * these fusions are determined by rules that differ based on the arcana and base levels of the
+ * ingredient personas. However, some special personas, called “treasure demons,” follow different
+ * rules when fused together with different personas. Also, there are usually multiple ways to
+ * fuse a specific persona, from two to more than a hundred.
+ * 
+ * Even though it is simple to calculate the result of a fusion between two specific personas,
+ * knowing all the possible pairs of personas that fuse into a specific persona is difficult
+ * without calculating the possible fusions between all personas. This is where the calculator
+ * may be useful: if a player wishes to fuse a specific persona p but does not know which pairs
+ * of personas will fuse together to create p.
+ * 
+ * Calculator Functions:
+ * 1. Provide information about a specific persona given its name.
+ * 2. List the personas in a specific arcana given the name of the arcana.
+ * 3. List all the arcana in the game.
+ * 4. List all the possible fusions for a specific persona given its name.
+ * 5. Provide the resulting persona of a fusion between two specific personas, given their names.
+ */
 public class FusionCalculator {
 
 	public static void main(String[] args) {
@@ -214,17 +243,3 @@ public class FusionCalculator {
 	}
 
 }
-/*
-*    - information about a specific persona given its name:
-*      get the data out of a master map of all persona
-*    - a list of all the arcana:
-*      get keyset of map from arcana to persona list
-*    - a list of all the personas in a specific arcana
-*      return a copy of the list storing the persona objects for the specified arcana
-*    - a list of all the persona pairs that will fuse into a specific persona
-*      look up in the map from persona name to set of pairs
-*    - the result of the fusion of two specific personas
-*      look up the result in the graph. if its not there then its an invalid fusion
-*    - all the fusions a specific persona can be in:
-*      get children of the persona node
-*/
